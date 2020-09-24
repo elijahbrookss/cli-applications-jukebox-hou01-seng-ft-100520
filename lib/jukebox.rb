fb851fb1cc01aec02d03c41d320ff42606cffb74
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-24 06:37:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,64 @@
-# Add your code here
+$songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+];
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.length.times{|index| puts "#{index + 1}. #{songs[index]}"}
+end
+
+def findSong(songs_list,song_name)
+  songs_list.each_with_index{|song, index| if song == song_name then return index end}
+ nil
+end
+
+def play(songs)
+  
+  numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  puts "Please enter a song name or number:"
+  choice = gets.strip;
+  if (numbers.include?(choice) or songs.include?(choice)) then
+    song = findSong(songs, choice) ? songs[findSong(songs, choice)] : songs[choice.to_i - 1]
+    puts "Playing #{song}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def run
+  while true do 
+    puts "Please enter a command:"
+    
+    choice = gets.strip;
+    case choice
+    when "play"
+      play($songs)
+    when "help"
+      help($songs)
+    when "list"
+      list($songs)
+    when "exit"
+      exit_jukebox
+      break
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
